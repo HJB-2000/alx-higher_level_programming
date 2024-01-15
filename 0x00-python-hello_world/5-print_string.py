bd753ed8bae85d0fd4f02ec108eb133137957743
Jassim Bahmida <jassim123bahmida@gmail.com>
2024-01-15 15:05:51 -0800

    /0x00-python-hello_world

---
+++ 0x00-python-hello_world/5-print_string.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+string = "Holberton School"
+print(string + string + string)
+print(string[:9])
---
+++ 0x00-python-hello_world/7-edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+i = "Holberton"
+a = i[:3]
+b = i[-2:]
+c = i[1:-1]
+print("First 3 letters: {}".format(a))
+print("Last 2 letters: {}".format(b))
+print("Middle word: {}".format(c))
