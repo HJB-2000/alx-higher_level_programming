329ce07a866ebd36ea27fb618ec3f96c6ea37dda
Jassim Bahmida <jassim123bahmida@gmail.com>
2024-01-16 15:45:36 -0800

    2

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,5 +2,10 @@
 
 for i in range(10):
     for j in range(i + 1, 10):
-        print("{:d}{:d}".format(i, j), end=", " if i != 8 or j != 9 else "\n")
+        combination = "{:d}{:d}".format(i, j)
+        if i != 8 or j != 9:
+            combination += ", "
+        else:
+            combination += "\n"
+        print(combination, end="")
 
