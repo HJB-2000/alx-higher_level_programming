3b3757d77ff52b6174ef60ad1b1f1aace52c9e93
Jassim Bahmida <jassim123bahmida@gmail.com>
2024-01-16 15:16:33 -0800

    1

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for char_code in range(ord('a'), ord('z') + 1):
     if char_code not in (ord('e'), ord('q')):
-        print(chr(char_code), end="")
-
+        print("{:c}".format(char_code), end="")
